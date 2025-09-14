6c4b420564277bc06b1badc3f187bb6f47dd2c1f
Arunkumar-Arjava <arunkumar.arjava@gmail.com>
2025-09-14 10:41:26 +0530

    3 pages complited

--- goddard/src/App.jsx
+++ goddard/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import LoginNew from './Components/AuthO/LoginNew';
+import ApplicationStatus from './Components/Admin/ApplicationStatus';
+import FormsRepository from './Components/Admin/FormsRepository';
+import InviteParent from './Components/Admin/InviteParent';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>goddard</h1>
-       
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginNew />} />
+          <Route path="/application-status" element={<ApplicationStatus />} />
+          <Route path="/admin-dashboard" element={<ApplicationStatus />} />
+          <Route path="/forms-repository" element={<FormsRepository />} />
+          <Route path="/invite-parent" element={<InviteParent />} />
+        </Routes>
+        <Toaster position="top-right" richColors />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
