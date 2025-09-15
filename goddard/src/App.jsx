3d2a47332aeabd3051cdbb89fabdb4fc30a5f611
Arunkumar-Arjava <arunkumar.arjava@gmail.com>
2025-09-15 16:35:17 +0530

    new change

--- goddard/src/App.jsx
+++ goddard/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
-import LoginNew from './Components/AuthO/LoginNew';
+import SelectSchool from './Components/AuthO/SelectSchool';
+import AdminDashboard from './Components/Admin/AdminDashboard';
 import ApplicationStatus from './Components/Admin/ApplicationStatus';
 import FormsRepository from './Components/Admin/FormsRepository';
 import InviteParent from './Components/Admin/InviteParent';
+import ParentDetails from './Components/Admin/ParentDetails';
+import ParentDashboard from './Components/parentFlow/ParentDashboard';
 import './App.css';
 
 function App() {
@@ -13,11 +16,14 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<LoginNew />} />
+          <Route path="/login" element={<SelectSchool />} />
           <Route path="/application-status" element={<ApplicationStatus />} />
-          <Route path="/admin-dashboard" element={<ApplicationStatus />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
           <Route path="/forms-repository" element={<FormsRepository />} />
           <Route path="/invite-parent" element={<InviteParent />} />
+          <Route path="/parent-details" element={<ParentDetails />} />
+          <Route path="/parent-dashboard" element={<ParentDashboard />} />
+
         </Routes>
         <Toaster position="top-right" richColors />
       </div>
